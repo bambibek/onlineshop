c6059e78b0b0e65131d516afe872ef2657fdf3e2
Bibek Bam <bibek2bam@gmail.com>
2022-01-07 19:34:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Button, TextField } from '@mui/material';
+import * as React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <div>
+        <Button variant="contained"> hello World</Button>
+
+      </div>
     </div>
   );
 }
